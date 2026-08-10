a487a398cc980541ba606b3fe6e165a683274716
furkanyazcicek <furkan.yazcicek@gmail.com>
2026-08-11 00:16:14 +0300

    Update ParentDashboard.jsx

--- src/pages/ParentDashboard.jsx
+++ src/pages/ParentDashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 export default function ParentDashboard() {
   const { user } = useAuth();
