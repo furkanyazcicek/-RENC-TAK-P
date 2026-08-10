02affbd0014fada7cd2223415af7919d7b8f6f10
furkanyazcicek <furkan.yazcicek@gmail.com>
2026-08-11 00:14:05 +0300

    Update and rename Register.jsx.jsx to Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 import { useNavigate, Link } from 'react-router-dom';
 
 export default function Register() {
