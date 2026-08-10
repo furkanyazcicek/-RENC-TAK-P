0071f351e06021426f5a6130f3c08256315bc03e
furkanyazcicek <furkan.yazcicek@gmail.com>
2026-08-11 00:15:42 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 import { useNavigate, Link } from 'react-router-dom';
 
 export default function Login() {
